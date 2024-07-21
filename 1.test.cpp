5ba891f8a63458853890efa0c4cd13c457951756
minilie <chensm57@mail2.sysu.edu.cn>
2024-07-21 17:46:31 +0800

    test for daily commit

---
+++ 1.test.cpp
@@ -0,0 +1,24 @@
+/*************************************************************************
+	> File Name: 1.test.cpp
+	> Author:Xin 
+	> Mail:2923262959@qq.com 
+	> Created Time: Sun 21 Jul 2024 05:45:30 PM CST
+ ************************************************************************/
+
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <string>
+#include <map>
+#include <set>
+#include <vector>
+using namespace std;
+
+int main() {
+    
+
+    return 0;
+}
